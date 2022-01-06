5ab7298af58caea2f457a4b93cea1b3892c66834
Jacob <jacobwamsley12@gmail.com>
2022-01-06 11:19:08 -0700

    Testing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('hello World')
+
+let backpack = []
+
+backpack.push('sword', 'postion')
+backpack.push('sheild')
+backpack.push('food')
+
+//console.log(backpack)
+
+let onBelt = backpack.splice(0,1)
+
+console.log(backpack)
+console.log(onBelt)
+console.log(`Backpack: `, backpack, `Belt: `, onBelt)
